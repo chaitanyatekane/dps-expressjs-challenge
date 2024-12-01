ab25014b6a593a5e085405b23ab9fe889a206eb1
Tekane Chaitanya <chaitanyatekne5@gmail.com>
2024-12-01 16:05:34 -0500

    feat: add authentication middleware to secure API routes

--- src/routes/project.routes.ts
+++ src/routes/project.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import * as projectController from '../controllers/project.controller';
+import authMiddleware from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get('/', projectController.getAllProjects);
-router.post('/', projectController.createProject);
-router.put('/:id', projectController.updateProject);
-router.delete('/:id', projectController.deleteProject);
+router.get('/', authMiddleware, projectController.getAllProjects);
+router.post('/', authMiddleware, projectController.createProject);
+router.put('/:id', authMiddleware, projectController.updateProject);
+router.delete('/:id', authMiddleware, projectController.deleteProject);
 
 export default router;
--- src/routes/report.routes.ts
+++ src/routes/report.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import * as reportController from '../controllers/report.controller';
+import authMiddleware from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get('/', reportController.getAllReports);
-router.get('/frequent-words', reportController.getReportsWithFrequentWords);
-router.post('/', reportController.createReport);
-router.put('/:id', reportController.updateReport);
-router.delete('/:id', reportController.deleteReport);
+router.get('/', authMiddleware, reportController.getAllReports);
+router.get(
+	'/frequent-words',
+	authMiddleware,
+	reportController.getReportsWithFrequentWords,
+);
+router.post('/', authMiddleware, reportController.createReport);
+router.put('/:id', authMiddleware, reportController.updateReport);
+router.delete('/:id', authMiddleware, reportController.deleteReport);
 
 export default router;

0b52bc1fe303ec57af31278d69b538cc32d33414
Tekane Chaitanya <chaitanyatekne5@gmail.com>
2024-12-01 13:51:00 -0500

    chore: initialize project with TypeScript and Express setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,8 +6,17 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// Middleware to parse JSON
 app.use(express.json());
 
+// Default route
+app.get('/', (req: Request, res: Response) => {
+	res.send('Welcome to the DPS Backend Challenge API!');
+});
+
+// Start the server
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+export default app;
